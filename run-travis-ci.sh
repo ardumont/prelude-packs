99a8e8ca7154c6ed393da7539016887a0dae5950
Antoine R. Dumont <eniotna.t@gmail.com>
2015-03-22 20:53:05 +0100

    Remove dead code

--- run-travis-ci.sh
+++ run-travis-ci.sh
@@ -7,7 +7,6 @@ CURRENT_EMACS=${EMACS:-$(which emacs)}
 echo "*** Emacs version ***"
 echo "CURRENT_EMACS = $CURRENT_EMACS"
 "$CURRENT_EMACS" --version
-#echo "CURRENT_CMD = $CURRENT_CMD"
 
 $CURRENT_EMACS --version
 
